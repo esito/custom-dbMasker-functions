aedbf846e535186e4dd90c31509a7a268b28f80e
Martin Holøs <mh@esito.no>
2021-11-02 13:19:22 +0100

    added first set of custom code

---
+++ conversions/ParseDigits.java
@@ -0,0 +1,25 @@
+package no.esito.anonymizer.conversions;
+
+import no.esito.anonymizer.IConversion;
+
+/**
+ * Simple example to parse digits from a string
+ */
+public class ParseDigits implements IConversion {
+
+	public static final String LABEL = "ParseDigits - simply remove all non-digits";
+
+	@Override
+	public Object convert(String input) {
+		if(input == null)
+			return "0";
+		char[] chars = input.toCharArray();
+		StringBuilder sb = new StringBuilder();
+		for (char c : chars) {
+			if(Character.isDigit(c))
+				sb.append(c);
+		}
+		return sb.length()==0?"0":sb.toString();
+	}
+
+}
